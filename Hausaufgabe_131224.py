497d772eb544771e64747b619bf2322ff167e45d
WojciechTechstarter <wojciech.olakowski@tn.techstarter.de>
2024-12-16 09:15:56 +0100

    Update Hausaufgabe_131224.py

--- Hausaufgabe_131224.py
+++ Hausaufgabe_131224.py
@@ -1,7 +1,7 @@
 
 date = input('Input a date following the example - YYYY.MM.DD: ')
 
-print(f'Der Typ von date ist: {type(date)}')
+print(f'The filetype is: {type(date)}')
 
 if date < '2024.11.25':
     print(f'The course has not started yet'),
@@ -10,7 +10,7 @@ elif date == '2024.11.25':
 elif date >= '2024.12.23' and date <= '2025.01.02':
     print('Christmas vacations'),
 elif date == '2025.02.25':
-    print(f'The end on Module 1'),
+    print(f'The end of Module 1'),
 elif date > '2025.11.25':
     print(f'The course has ended')
 else: 
